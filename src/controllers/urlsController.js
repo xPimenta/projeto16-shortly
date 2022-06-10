e9e50500461a430caa87f735e233fa207775f4f0
Pimenta <mateuspimartins@hotmail.com>
2022-06-10 15:29:20 -0300

    get url params

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -23,3 +23,49 @@ export async function shortenURL(req, res) {
         res.status(500).send(e)
     }
 }
+
+export async function getUrlById(req, res) {
+    const { id } = req.params
+    try {
+        const query = await connection.query(
+            `SELECT * FROM links
+            WHERE links.id = $1
+            `,
+            [id]
+        )
+
+        if (!query.rows[0]) return res.sendStatus(404)
+
+        const { shortUrl, url } = query.rows[0]
+
+        return res.status(200).send({ id: id, shortUrl: shortUrl, url: url })
+    } catch (e) {
+        res.status(500).send(e)
+    }
+}
+
+export async function redirectToUrl(req, res) {
+    const { shortUrl } = req.params
+    try {
+        const exists = await connection.query(
+            `SELECT * FROM links
+            WHERE links."shortUrl" = $1
+            `,
+            [shortUrl]
+        )
+        if (!exists.rows[0]) return res.sendStatus(404)
+
+        const query = await connection.query(
+            `UPDATE links
+            SET visits = visits + 1
+            WHERE links."shortUrl" = $1
+            `,
+            [shortUrl]
+        )
+
+        const { url } = exists.rows[0]
+        return res.redirect(url)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+}
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -5,11 +5,15 @@ import {
     validateURL,
 } from "../middlewares/validation.js"
 import {
+    getUrlById,
+    redirectToUrl,
     shortenURL,
 } from "../controllers/urlsController.js"
 
 const urlsRouter = Router()
 
 urlsRouter.post("/urls/shorten", validateURL, validateToken, shortenURL)
+urlsRouter.get("/urls/open/:shortUrl", redirectToUrl)
+urlsRouter.get("/urls/:id", getUrlById)
 
 export default urlsRouter
